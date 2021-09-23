a6d6406dac08333bcb384fe3b654dfecf665cd98
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-24 00:18:23 +0530

    heading change

--- src/components/News.js
+++ src/components/News.js
@@ -17,8 +17,12 @@ export class News extends Component {
         category : PropTypes.string
     }
 
-    constructor(){
-        super();
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    constructor(props){
+        super(props);
         //console.log("constructor..", KEY);
         this.state = {
             articles : [],
@@ -27,6 +31,7 @@ export class News extends Component {
            // pageSize : 20,
             totalResults : 0
         }
+        document.title = this.capitalizeFirstLetter(`${this.props.category} - NewsMonkey`);
     }
 
     //getNews = async function(){
@@ -82,7 +87,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h1 className="text-center" style={{margin: '10px 0px'}}>NewsMonkey - Top Headlines</h1>
+                <h1 className="text-center" style={{margin: '10px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(`${this.props.category}`)} Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {                
